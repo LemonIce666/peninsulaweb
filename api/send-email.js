7230369d1f8a9f94c95e8fbb3f4a817df20c5c19
张璟聪 <zhangjingcong@zhangjingcongdeMacBook-Pro.local>
2025-01-02 22:19:29 +0800

    Message

--- api/send-email.js
+++ api/send-email.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 module.exports = async (req, res) => {
     // 设置 CORS 头，允许来自 GitHub Pages 的请求
-    res.setHeader('Access-Control-Allow-Origin', 'https://lemonice666.github.io'); // 允许来自 GitHub Pages 的请求
+    res.setHeader('Access-Control-Allow-Origin', 'https://lemonice666.github.io/peninsulaweb/'); // 允许来自 GitHub Pages 的请求
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
